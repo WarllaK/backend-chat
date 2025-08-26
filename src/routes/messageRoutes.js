5144bc21bf581121cd457318a0c59ce67515a03a
mikael <mikael.st15@protonmail.com>
2025-08-26 20:18:32 -0300

    feat: added id param to route /messages

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const messageController = require('../controllers/messageController');
 const auth = require('../middleware/auth');
 
-router.post('/messages', auth, messageController.createMessage);
+router.post('/messages/:id', auth, messageController.createMessage);
 router.get('/messages', auth, messageController.getMessages);
 
 module.exports = router;
